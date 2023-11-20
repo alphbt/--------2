e6c0a6afdc725cbc5e7bc7168bddf57f19f3cb0d
alphbt <dashakaverina6@gmail.com>
2023-11-20 23:28:32 +0300

    add timer

--- Solution.cpp
+++ Solution.cpp
@@ -6,7 +6,13 @@ int main(int argc, char * argv[])
     Parser parser = Parser(argc, argv);
     FunctionParameters params = parser.Parse();
     FunctionRealization solve = FunctionRealization(params);
+    time_t start, end;
+    time(&start);
     solve.ComputeFunction();
+    time(&end);
+    double time_taken = double(end - start); 
+    cout << "Time taken by program is : " << fixed 
+        << time_taken << " sec " << endl;
     solve.SaveFunctionsValuesToTxtFiles();
     return 0;
 }
