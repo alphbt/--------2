34ace5bef54e2d439e884009cd9eb1c71e9cdf72
alphbt <dashakaverina6@gmail.com>
2023-11-21 23:36:08 +0300

    change timer

--- Solution.cpp
+++ Solution.cpp
@@ -1,18 +1,19 @@
 #include "Parser.h"
 #include "FunctionRealization.h"
+#include <iomanip>
 
 int main(int argc, char * argv[])
 {
     Parser parser = Parser(argc, argv);
     FunctionParameters params = parser.Parse();
     FunctionRealization solve = FunctionRealization(params);
-    time_t start, end;
-    time(&start);
+    clock_t start, end;
+    start = clock();
     solve.ComputeFunction();
-    time(&end);
-    double time_taken = double(end - start); 
+    end = clock();
+    double time_taken = double(end - start) / CLOCKS_PER_SEC; 
     cout << "Time taken by program is : " << fixed 
-        << time_taken << " sec " << endl;
+        << time_taken << setprecision(5) << " sec " << endl;
     solve.SaveFunctionsValuesToTxtFiles();
     return 0;
 }
